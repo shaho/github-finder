0b1cb8f69fdcc7dac7f74b4634865ea380a494c6
shaho <toofani@gmail.com>
2020-01-08 15:50:27 +0330

    Add webpack.config and babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+module.exports = {
+  mode: "development",
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
