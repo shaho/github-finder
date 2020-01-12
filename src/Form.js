9b96a71c8c3896bde0d95d60414d4a6b8eb74f28
shaho <toofani@gmail.com>
2020-01-12 19:33:04 +0330

    Add handleKeyup event linstener

--- src/Form.js
+++ src/Form.js
@@ -6,16 +6,28 @@ class Form {
     this.searchTerm = "";
 
     this.searchInput = document.querySelector('input[name="search"]');
-    this.searchInput.addEventListener("keyup", this.handleKeyup);
+    this.searchInput.addEventListener("keyup", () => this.handleKeyup(event));
 
     this.submitButton = document.querySelector('button[type="submit"]');
     this.submitButton.disabled = !this.searchTerm;
+
+    // this.form = document.querySelector("form");
+    // this.form.addEventListener("submit", this.handleSubmit());
   }
-  handleKeyup(e) {
-    this.searchTerm = e.target.value.trim();
+
+  // keyUp Handler
+  handleKeyup(event) {
+    this.searchTerm = event.target.value.trim();
     this.API_URL = `${API_URL}/${this.searchTerm}`;
+    console.log(this.API_URL);
     this.submitButton.disabled = !this.searchTerm;
   }
+
+  // handlesubmit handler
+  // handleSubmit(e) {
+  //   e.preventDefault();
+  //   console.log(e);
+  // }
 }
 
 export default Form;
